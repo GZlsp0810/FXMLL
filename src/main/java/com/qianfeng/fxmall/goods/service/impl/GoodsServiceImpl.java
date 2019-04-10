2cf0213aeedcf0d1ac86337023a5a0dced2d317a
GZlsp0810 <527854950@qq.com>
2019-04-10 18:48:39 +0800

    初始提交1

--- src/main/java/com/qianfeng/fxmall/goods/service/impl/GoodsServiceImpl.java
+++ src/main/java/com/qianfeng/fxmall/goods/service/impl/GoodsServiceImpl.java
@@ -5,16 +5,20 @@ import com.qianfeng.fxmall.goods.dao.IGoodsDAO;
 import com.qianfeng.fxmall.goods.dao.impl.GoodsDAOImpl;
 import com.qianfeng.fxmall.goods.dto.WxbGood;
 import com.qianfeng.fxmall.goods.service.IGoodsService;
+import org.apache.log4j.Logger;
+
 
 import java.util.List;
 
 public class GoodsServiceImpl implements IGoodsService {
 
     private IGoodsDAO goodsDAO = new GoodsDAOImpl();
-
+   private Logger logger =Logger.getLogger(GoodsServiceImpl.class);
     @Override
     public List<WxbGood> queryGoodsByPage(Integer page) throws Exception {
+              logger.debug("queryGoodsByPage:page"+page);
         if (page < 1) {
+            logger.warn(page>1);
             throw new IndexOutOfBoundsException("页码不能小于1");
         }
 
@@ -22,6 +26,14 @@ public class GoodsServiceImpl implements IGoodsService {
         int index = (page-1)* SystemConstantsUtils.Page.PAGE_SIZE;
 
         List<WxbGood> goods = goodsDAO.queryGoodsByPage(index);
+       StringBuilder builder=new StringBuilder();
+        if (goods!=null&&!goods.isEmpty()){
+            for ( WxbGood good:goods
+                 ) {
+                builder.append(good.getGoodName()+",");
+            }
+            logger.debug("queryGoodsByPage,return:"+builder.toString());
+        }
         return goods;
     }
 
